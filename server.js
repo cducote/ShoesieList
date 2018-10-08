2ea18499311f5605698f4807dc3c8244e004fddc
Chris Ducote <chrisd@slappy.local>
2018-10-08 14:24:30 -0400

    express and react set up

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+require('dotenv').config()
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+const connection = mongoose.connection
+
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully')
+})
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err)
+})
+
+var indexRouter = require('./routes');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -12,9 +25,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 module.exports = app;
